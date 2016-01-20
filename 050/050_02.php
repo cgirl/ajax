e7d7ef86e2ad769e66aa4223a4dec7f9b7984bbf
cgirl <1579090779@qq.com>
2016-01-20 15:52:44 +0800

    远古时代的Ajax

--- 050/050_02.php
+++ 050/050_02.php
@@ -1,6 +1,12 @@
 <?php
 header("content-type:text/html;charset=utf-8");
 
-print_r($_POST);
-echo 'reg ok!';
+//print_r($_POST);
+//echo 'reg ok!';
+
+//经过运算，注册成功，$res=1
+$res = 1;
 ?>
+<script>
+parent.document.getElementById('regres').innerHTML = '注册成功';
+</script>
